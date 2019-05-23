c1c22ddd9fe024f02550de4251298d50ee102a1a
MedMekss <45265033+MedMekss@users.noreply.github.com>
2019-05-23 14:35:47 +0200

    Homepage
    Homepage (not finished)

--- todo/views.py
+++ todo/views.py
@@ -10,7 +10,7 @@ from . import models
 
 # /todo/
 class IndexView(TemplateView):
-    template_name = 'todo/template.html'
+    template_name = 'todo/home.html'
 
 
 class CategoryAddView(FormView):
