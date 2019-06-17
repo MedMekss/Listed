401ed9d13de732da1e65272c42b0c0f179fc851b
Jared <hall@fira.nl>
2019-06-17 15:27:30 +0200

    DatePicker Update
    DatePicker will show acurate dates, and restrict from doing illegal manouvers.

--- todo/forms.py
+++ todo/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import widgets
 from .models import CheckList, Category, Item
+from django.utils.timezone import now, timezone
 
 
 class CategoryForm(forms.ModelForm):
@@ -8,8 +9,10 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ['category_name', 'color']
         widgets = {
-            'category_name': forms.TextInput(attrs={'placeholder': 'Enter Category Name', 'class': 'overflowscroll w-100 textinput no-border transpearant'}),
-            'color': forms.TextInput(attrs={'type':'color', 'class':'nopad transpearant no-border', 'style':'margin-left: 8px; margin-bottom:6px'})
+            'category_name': forms.TextInput(attrs={'placeholder': 'Enter Category Name',
+                                                    'class': 'overflowscroll w-100 textinput no-border transpearant'}),
+            'color': forms.TextInput(attrs={'type': 'color', 'class': 'nopad transpearant no-border',
+                                            'style': 'margin-left: 8px; margin-bottom:6px'})
         }
         labels = {'Choose Color'}
 
@@ -19,8 +22,12 @@ class ChecklistForm(forms.ModelForm):
         model = CheckList
         fields = ['checklist_name', 'category', 'start_date', 'end_date']
         widgets = {
-            'checklist_name': forms.TextInput(attrs={'placeholder': 'Enter Checklist Name', 'class': 'overflowscroll w-100 no-border transpearant'}),
-            'category':forms.Select(attrs={'class':'', 'style': 'margin-left:10px' }),
-            'start_date': forms.TextInput(attrs={'type': 'date', 'style': 'margin-left:4px', 'min':{}}),
-            'end_date': forms.TextInput(attrs={'type': 'date', 'style': 'margin-left:10px; padding-right:2px; margin-top:10px'})
+            'checklist_name': forms.TextInput(
+                attrs={'placeholder': 'Enter Checklist Name', 'class': 'overflowscroll w-100 no-border transpearant'}),
+            'category': forms.Select(attrs={'class': '', 'style': 'margin-left:10px'}),
+            'start_date': forms.TextInput(
+                attrs={'type': 'date', 'min': now().date(), 'style': 'margin-left:4px', 'value': now().date()}),
+            'end_date': forms.TextInput(attrs={'type': 'date', 'min': now().date(),
+                                               'style': 'margin-left:10px; padding-right:2px; margin-top:10px',
+                                               'value': now().date()})
         }
